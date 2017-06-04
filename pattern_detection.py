56668e3cd20c0193bb2b83f54b971036cddf8a34
rhdzmota <rhdzmota@mxquants.com>
2017-06-04 11:11:44 -0500

    fix modules for Danny

--- pattern_detection.py
+++ pattern_detection.py
@@ -4,7 +4,22 @@ Pattern detection in financial time-series data.
 @author: Rodrigo Hernández-Mota
 """
 
+import os
+os.chdir("/media/rhdzmota/Data/Files/github_mxquants/usdmxnForecast")
 import numpy as np
 import pandas as pd
 import quanta as mx
 import matplotlib.pyplot as plt
+
+
+def lagMatrix(df, lag=5):
+    """Return lag matrix for a given time series (dataframe)."""
+    n = len(df)
+    input_data = [df.iloc[i:(n - lag + i + 1)].values for i in range(lag)]
+    input_data = pd.DataFrame(np.concatenate(input_data, 1))
+    return input_data
+
+
+# Test
+df = mx.data.getBanxicoSeries("usdmxn_fix")
+lags = lagMatrix(df)
