b01ce2b7b23a660d08f7164465cf6245397105f8
rhdzmota <rhdzmota@mxquants.com>
2017-06-04 10:07:20 -0500

    add pattern_detection first script and gitignore

---
+++ pattern_detection.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Pattern detection in financial time-series data.
+@author: Rodrigo Hernández-Mota
+"""
+
+import numpy as np
+import pandas as pd
+import quanta as mx
+import matplotlib.pyplot as plt
